91509179b7f6df8ec0ac3f66ea6d75a2bd4472f7
Jijo <jijojacob988@gmail.com>
2025-10-14 10:28:24 +0530

    feat: add controller to verify if user is authenticated

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -56,4 +56,7 @@ export const login = async (req, res)=>{
     }
 }
 
-// controller to get user data
+// controller to check if user is authenticated
+export const checkAuth = (req, res)=>{
+    res.json({success: true, user: req.user});
+}
