5bc5892c2edb5cb311769e84fe2ff4e10c4d9f97
Jijo <jijojacob988@gmail.com>
2025-10-15 10:29:25 +0530

    created the message controller function

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,3 +1,27 @@
+import Message from "../models/Message.js";
+import User from "../models/User.js";
 
 
-// 
+// Get all users except the logged-in user
+export const getUsersForSidebar = async (req, res)=>{
+    try {
+        const userId = req.user._id;
+        const filtetedUsers = await User.find({_id: {$ne: userId}}).select("-password");
+
+        // count of unseen messages from each user
+        const unseenMessages = {}
+        const promises = filtetedUsers.map(async (user)=>{
+           const messages = await Message.find({senderId: user._id, receiverId: userId, seen: false});
+           if(messages.length > 0){
+            unseenMessages[user._id] = messages.length;
+           }
+        })
+        await Promise.all(promises);
+
+        res.json({success: true, users: filtetedUsers, unseenMessages})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+        
+    }
+}
