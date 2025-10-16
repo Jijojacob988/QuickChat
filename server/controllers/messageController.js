be1ef5ed03908ca78c741db4da994fed4afcd30e
Jijo <jijojacob988@gmail.com>
2025-10-16 10:47:13 +0530

    feat: add function to send new message and store it in database

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,5 +1,6 @@
 import Message from "../models/Message.js";
 import User from "../models/User.js";
+import cloudinary from "../lib/cloudinary.js";
 
 
 // Get all users except the logged-in user
@@ -55,6 +56,33 @@ export const markMessageAsSeen = async (req, res) =>{
         const { id } = req.params;
         await Message.findByIdAndUpdate(id, {seen: true});
         res.json({success: true})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+
+// send messsage to the selected user
+export const sendMessage = async (req, res) =>{
+    try {
+        const {text, image} = req.body;
+
+        const receiverId = req.params.id;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+           const uploadResponse = await cloudinary.uploader.upload(image)
+           imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = await Message.create({
+            senderId, receiverId, text, image: imageUrl
+        })
+
+        res.json({success: true, newMessage});
+
+
     } catch (error) {
         console.log(error.message);
         res.json({success: false, message: error.message})
