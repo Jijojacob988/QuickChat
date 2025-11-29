07dd727ea4fdb8e95856c3572f5644d8da584573
Jijo <jijojacob988@gmail.com>
2025-11-29 14:38:18 +0530

    feat: add functions to get messages, send messages, and subscribe to messages for selected user

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -29,6 +29,48 @@ export const ChatProvider = ({children})={
         }
     }
 
+    //function to get messages for selected user
+    const getMessages = async (userId)=>{
+        try {
+        const { data } = await axios.get(`/api/messages/${userId}`);
+        if (data.success){
+            setMessages(data.messages);
+        }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    // function to send message to selected user
+    const sendMessage = async (messageData)=>{
+       try {
+        const {data} = await axios.post(`/api/messages/send${selectedUser._id}`, messageData);
+        if(data.success){
+            setMessages((prevMessages)=>[...prevMessages, data.newMessage]);
+        }else{
+            toast.error(data.message);
+        }
+       } catch (error) {
+           toast.error(error.message);
+       }
+    }
+
+    // function to subscribe to messages for selected user
+    const subscribeToMessages = async ()=>{
+      if(!socket) return;
+        socket.on("newMessage", (newMessage)=>{
+            if(selectedUser && newMessage.senderId === selectedUser._id){
+                newMessage.seen = true;
+                setMessages((prevMessages)=> [...prevMessages, newMessage]);
+                axios.put(`/api/messages/mark/${newMessage._id}`);
+            }else{
+                setUnseenMessages((prevUnseenMessages)=>({
+                    ...prevUnseenMessages, [newMessage.senderId] : prevUnseenMessages[newMessage.senderId] ? prevUnseenMessages[newMessage.senderId] + 1 : 1
+                }))
+            }
+        }) 
+    }
+
 
     const value = {
 
